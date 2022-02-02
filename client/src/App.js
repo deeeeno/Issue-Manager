8732c4164f965db84d57b840a14c9e570e3503ef
deeeeno <ksh0512dog@gmail.com>
2022-02-03 00:13:53 +0900

    async edit

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {userById,requestQuery} from './graphql/request';
 
-const data = requestQuery(userById('dino','id','password','nickName'));
-console.log(data);
+requestQuery(userById('dino','id','password','nickName')).then(data=>console.log(data));
 function App() {
 
   return (
--- client/src/graphql/request.js
+++ client/src/graphql/request.js
@@ -27,20 +27,17 @@ export const commentByIssueSeq = function(seq,...rest){
         ${rest.join(' ')}
     }`;
 }
-
 export const requestQuery = async function(...qs){
     const query = `{
         ${qs.join('\n')}
-    }`
-    console.log(qs);
-    console.log(JSON.stringify({query}));
-    const response = await fetch(URL,
+    }` 
+    const res = await fetch(URL,
         {
             method:'POST',
             headers:{'Content-Type':'application/json'},
             body:JSON.stringify({query})
         });
-    const json = await response.json();
-    console.log(json['data']);
+    const json = await res.json();
     return json['data'];
+
 }
