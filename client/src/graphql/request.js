a893db4b0889b5533b96a9d5254b5961a862f55c
deeeeno <ksh0512dog@gmail.com>
2022-02-03 11:17:57 +0900

    add mutation request function

--- client/src/graphql/request.js
+++ client/src/graphql/request.js
@@ -2,9 +2,9 @@ import {User,Issue,Comment} from '../data/structure';
 
 const URL = 'http://localhost:4000';
 
-export const requestQuery = async function(...qs){
+export const requestQuery = async function(...querys){
     const query = `{
-        ${qs.join('\n')}
+        ${querys.join('\n')}
     }` 
     const res = await fetch(URL,
         {
@@ -14,5 +14,18 @@ export const requestQuery = async function(...qs){
         });
     const json = await res.json();
     return json['data'];
+}
 
+export const requestMutation = async function(...mutations){
+    const mutation = `{
+        ${mutations.join('\n')}
+    }` 
+    const res = await fetch(URL,
+        {
+            method:'POST',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({mutation})
+        });
+    const json = await res.json();
+    return json['data'];
 }
