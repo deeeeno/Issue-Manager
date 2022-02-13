a079f5c58621fb0c2a673b31944d7fd605496761
deeeeno <ksh0512dog@gmail.com>
2022-02-13 23:47:11 +0900

    project 관련 graphql 추가

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,12 +1,14 @@
 import {userById,userBySeq} from '../data/users';
 import {issueById,issueByUserSeq,createIssue} from '../data/issues';
+import {getProjectAll,getProjectBySeq} from '../data/projects';
 import {commentByIssueSeq} from '../data/comments';
 export const rootValue = {
     userById: ({id},context) => userById(id,context),
     userBySeq: ({seq}) => userBySeq(seq),
     issueById: ({id}) => issueById(id),
     issueByUserSeq: ({userseq}) => issueByUserSeq(userseq),
+    getProjectAll: () => getProjectAll(),
+    getProjectBySeq: ({seq}) => getProjectBySeq(seq),
     commentByIssueSeq: ({issueseq}) => commentByIssueSeq(issueseq),
     createIssue:({issue}) => createIssue(issue)
-    //createIssue:({id,reporter_seq,asignee_seq,status,priority,title,description}) => createIssue(id,reporter_seq,asignee_seq,status,priority,title,description)
 }
