a57c0e3ba4b18d049e0b958eaba180952a7164e4
deeeeno <ksh0512dog@gmail.com>
2022-02-15 17:22:44 +0900

    Graphql schema.resolve

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,14 +1,26 @@
 import {userById,userBySeq} from '../data/users';
-import {issueById,issueByUserSeq,createIssue} from '../data/issues';
-import {getProjectAll,getProjectBySeq} from '../data/projects';
+import {issueById,issuesByAsigneeSeq,issuesByReporterSeq,createIssue} from '../data/issues';
+import {projectsAll,getProjectBySeq} from '../data/projects';
 import {commentByIssueSeq} from '../data/comments';
 export const rootValue = {
-    userById: ({id},context) => userById(id,context),
+    userById: ({id}) => userById(id),
     userBySeq: ({seq}) => userBySeq(seq),
+    issuesByAsigneeSeq: ({asignee_seq}) => issuesByAsigneeSeq(asignee_seq),
+    issuesByReporterSeq: ({reporter_seq}) => issuesByReporterSeq(reporter_seq),
     issueById: ({id}) => issueById(id),
-    issueByUserSeq: ({userseq}) => issueByUserSeq(userseq),
-    getProjectAll: () => getProjectAll(),
-    getProjectBySeq: ({seq}) => getProjectBySeq(seq),
+    projectsAll: () => projectsAll(),
+    projectBySeq: ({seq}) => projectBySeq(seq),
     commentByIssueSeq: ({issueseq}) => commentByIssueSeq(issueseq),
-    createIssue:({issue}) => createIssue(issue)
+    createUser:({user}) => createUser(user),
+    updateUser:({user}) => updateUser(user),
+    deleteUser:({user}) => deleteUser(user),
+    createProject:({project}) => createProject(project),
+    updateProject:({project}) => updateProject(project),
+    deleteProject:({project}) => deleteProject(project),
+    createIssue:({issue}) => createIssue(issue),
+    updateIssue:({issue}) => updateIssue(issue),
+    deleteIssue:({issue}) => deleteIssue(issue),
+    createComment:({comment}) => createComment(comment),
+    updateComment:({comment}) => updateComment(comment),
+    deleteComment:({comment}) => deleteComment(comment),
 }
--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -8,6 +8,12 @@ type User{
     nickName:String!
     create_datetime:String!
 }
+input UserInput{
+    id:String!
+    password:String!
+    nickName:String!
+}
+
 type Project{
     seq:Int!
     name:String!
@@ -15,6 +21,12 @@ type Project{
     issue_count:Int!
     create_datetime:String!
 }
+input ProjectInput{
+    name:String!
+    symbol:String!
+    issue_count:Int!
+}
+
 type Issue{
     seq:Int!
     id:String!
@@ -39,7 +51,14 @@ input IssueInput{
 }
 
 type Comment{
-    index:Int!
+    index:Float!
+    issue_seq:Int!
+    commenter_seq:Int!
+    description:String!
+    create_datetime:String!
+}
+input CommentInput{
+    index:Float!
     issue_seq:Int!
     commenter_seq:Int!
     description:String!
@@ -48,13 +67,26 @@ type Comment{
 type Query{
     userById(id:String!):User
     userBySeq(seq:Int!):User!
+    issuesByAsigneeSeq(asignee_seq:Int!):[Issue]!
+    issuesByReporterSeq(reporter_seq:Int!):[Issue]!
+    issuesByProjectSeq(project_seq:Int!):[Issue]!
     issueById(id:Int!):Issue!
-    issueByUserSeq(userseq:Int!):[Issue]
-    getProjectAll():[Project]!
-    getProjectBySeq(seq:Int!):Project
+    projectsAll():[Project]!
+    projectBySeq(seq:Int!):Project
     commentByIssueSeq(issueseq:Int!):[Comment]
 }
 type Mutation{
+    createUser(user:UserInput!):Int
+    updateUser(user:UserInput!):User
+    deleteUser(user:UserInput!):Int
+    createProject(project:ProjectInput!):Int
+    updateProject(project:ProjectInput!):Project
+    deleteProject(project:ProjectInput!):Int
     createIssue(issue:IssueInput!):Int
+    updateIssue(issue:IssueInput!):Issue
+    deleteIssue(issue:IssueInput!):Int
+    createComment(comment:CommentInput!):Int
+    updateComment(comment:CommentInput!):Comment
+    deleteComment(comment:CommentInput!):Int
 }
 `);
